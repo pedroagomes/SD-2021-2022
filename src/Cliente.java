50f4147bd17e674f23584e94208c5843fec395c5
AstyuJack <joao_idolos@hotmail.com>
2022-01-10 00:17:14 +0000

    00:17 10/01

--- src/Cliente.java
+++ src/Cliente.java
@@ -8,9 +8,8 @@ public class Cliente {
 
     public static void main(String[] args){
 
-        Socket s = null;
         try {
-            s = new Socket("localhost",12345);
+            Socket s = new Socket("localhost",12345);
 
             DataOutputStream outS = new DataOutputStream(new BufferedOutputStream(s.getOutputStream()));
             outS.writeUTF("joao");
