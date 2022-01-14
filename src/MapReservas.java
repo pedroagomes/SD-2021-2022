3094d7568817b6d03790769fb0be9f642512b1cd
AstyuJack <joao_idolos@hotmail.com>
2022-01-14 20:27:31 +0000

    20:27 14/01

--- src/MapReservas.java
+++ src/MapReservas.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -5,4 +6,40 @@ public class MapReservas {
     private HashMap<Integer,Reserva> reservas;
     private int contador;
     private ReentrantLock lock;
+    private LocalDate diaActual;
+
+    public MapReservas(){
+        this.reservas = new HashMap<>();
+        this.contador = 1;
+        this.lock = new ReentrantLock();
+        this.diaActual = LocalDate.now();
+    }
+
+    public MapReservas(LocalDate diaInicial){
+        this.reservas = new HashMap<>();
+        this.contador = 0;
+        this.lock = new ReentrantLock();
+        this.diaActual = diaInicial;
+    }
+
+    public LocalDate getDiaActual(){
+        try{
+            lock.lock();
+            return diaActual;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void encerrarDia(){
+
+    }
+
+    public int fazerReserva(){
+        //retorna 0, se falhar
+    }
+
+    public boolean cancelarReserva(){
+
+    }
 }
