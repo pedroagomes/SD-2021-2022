ffdc2d547a730fd1cf5630ae119e9d1f82ea6b15
AstyuJack <joao_idolos@hotmail.com>
2022-01-15 17:42:20 +0000

    17:42 15/01

--- src/ListaCapacidades.java
+++ src/ListaCapacidades.java
@@ -4,9 +4,11 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class ListaCapacidades {
     private ArrayList<Capacidade> listaCapacidades;
+    private final int maxCapacidade;
     private ReentrantLock lock;
 
-    public ListaCapacidades(){
+    public ListaCapacidades(int maxCapacidade){
+        this.maxCapacidade = maxCapacidade;
         this.listaCapacidades = new ArrayList<>();
         this.lock = new ReentrantLock();
     }
@@ -14,14 +16,25 @@ public class ListaCapacidades {
     public Capacidade get(LocalDate dia){
         try {
             lock.lock();
+            int i;
             int size = listaCapacidades.size();
-            for(int i = 0; i < size; i++)
+            for(i = 0; i < size; i++)
                 if(listaCapacidades.get(i).getDia().equals(dia))
                     return listaCapacidades.get(i);
-            return null;
+            add(dia);
+            // verificar se quando não existe registo para o dia, está a criar e a devolver o correcto
+            return listaCapacidades.get(i);
         } finally {
             lock.unlock();
         }
+    }
 
+    public void add(LocalDate dia){
+        try {
+            lock.lock();
+            listaCapacidades.add(new Capacidade(dia,0, this.maxCapacidade));
+        } finally {
+            lock.unlock();
+        }
     }
 }
